5e58d8ed492df4f23e58731171ec786dd7fde805
tisacra <tisacra.i.c@gmail.com>
2020-12-12 12:47:10 +0900

    Comvolution update

--- test_tisaNET/test_tisaNET.cpp
+++ test_tisaNET/test_tisaNET.cpp
@@ -9,21 +9,34 @@ int main()
     tisaNET::Data_set test_data;
     //test_data.data = { {0,0,0},{0,0,1},{0,1,0},{0,1,1},{1,0,0},{1,0,1},{1,1,0},{1,1,1} };
     //test_data.answer = { {0,1},{1,0},{0,1},{0,1},{1,0},{1,0},{0,1},{1,0} };
-    tisaNET::load_MNIST("..\\..\\MNIST",train_data,test_data,50000,10000,false);
-    
+    tisaNET::load_MNIST("..\\..\\..\\..\\MNIST",train_data,test_data,50,10,false);
+
     tisaNET::Model model;
-    /*
-    model.Create_Layer(784,INPUT);
-    model.Create_Layer(32, RELU);
-    model.Create_Layer(32, SIGMOID);
+
+    /**/
+    std::vector<std::vector<double>> filter1 = { {1.0, 0.0, 0.0, 0.0, 1.0},
+                                                 {0.0, 0.5, 0.7, 0.5, 0.0},
+                                                 {0.0, 0.7, 1.0, 0.7, 0.0},
+                                                 {0.0, 0.5, 0.7, 0.5, 0.0},
+                                                 {1.0, 0.0, 0.0, 0.0, 1.0} };
+
+    std::vector<std::vector<double>> filter2 = { {1.0, 0.3, 1.0},
+                                                 {0.3, 1.0, 0.3},
+                                                 {1.0, 0.3, 1.0} };
+    
+    model.Create_Comvolute_Layer(28,28,filter1,2);
+    model.Create_Comvolute_Layer(11,11,filter1);
+    model.Create_Layer(40, RELU);
+    model.Create_Layer(20, SIGMOID);
     model.Create_Layer(10, SOFTMAX);
     model.initialize();
-    */
-    model.load_model("mnist0910_2.tp");
+    
+    /**/
+    //model.load_model("mnist_1212_2.tp");
 
     model.monitor_accuracy(true);
-    model.logging_error("log_mnist0910_2.csv");
-    model.train(0.01,train_data,test_data,5,10,CROSS_ENTROPY_ERROR);
-    model.save_model("mnist0910_2.tp");
+    //model.logging_error("log_mnist1211.csv");
+    model.train(0.05,train_data,test_data,10,10,CROSS_ENTROPY_ERROR);
+    model.save_model("mnist_1212_2.tp");
     return 0;
 }
