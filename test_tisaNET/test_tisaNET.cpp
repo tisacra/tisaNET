6532c140a566f0d1ab3bc1fd3c36f43c99770235
tisacra <tisacra.i.c@gmail.com>
2020-12-27 00:09:03 +0900

    bug fix

--- test_tisaNET/test_tisaNET.cpp
+++ test_tisaNET/test_tisaNET.cpp
@@ -4,12 +4,14 @@
 int main()
 {
     tisaNET::Data_set train_data;
-    //train_data.data = { {0,0,0},{0,0,1},{0,1,0},{0,1,1},{1,0,0},{1,0,1},{1,1,0},{1,1,1} };
-    //train_data.answer = { {0,1},{1,0},{0,1},{0,1},{1,0},{1,0},{0,1},{1,0} };
     tisaNET::Data_set test_data;
-    //test_data.data = { {0,0,0},{0,0,1},{0,1,0},{0,1,1},{1,0,0},{1,0,1},{1,1,0},{1,1,1} };
-    //test_data.answer = { {0,1},{1,0},{0,1},{0,1},{1,0},{1,0},{0,1},{1,0} };
-    tisaNET::load_MNIST("..\\..\\..\\..\\MNIST", train_data, test_data, 50000, 10000, false);
+    /*
+    train_data.data = { {0,0,0},{0,0,1},{0,1,0},{0,1,1},{1,0,0},{1,0,1},{1,1,0},{1,1,1} };
+    train_data.answer = { {0,1},{1,0},{0,1},{0,1},{1,0},{1,0},{0,1},{1,0} };
+    test_data.data = { {0,0,0},{0,0,1},{0,1,0},{0,1,1},{1,0,0},{1,0,1},{1,1,0},{1,1,1} };
+    test_data.answer = { {0,1},{1,0},{0,1},{0,1},{1,0},{1,0},{0,1},{1,0} };
+    */
+    tisaNET::load_MNIST("..\\..\\..\\..\\MNIST", train_data, test_data, 1000, 100, false);
 
     tisaNET::Model model;
 
@@ -24,26 +26,30 @@ int main()
                                                  {0.3, 1.0, 0.3},
                                                  {1.0, 0.3, 1.0} };
     */
-
-    //model.Create_Layer(784,INPUT);
+    //MNISTは784
+    //model.Create_Layer(3,INPUT);
     /**/
-    int input1[3] = { 28,28,1};
+    int input1[3] = {28,28,1};
     int filter1[3] = {5,5,1};
-    model.Create_Comvolute_Layer(input1,filter1,10,2);
-    int input2[3] = {11,11,10};
+    model.Create_Comvolute_Layer(input1,filter1,5,2);
+    int input2[3] = {12,12,5};
     int filter2[3] = { 5,5,1 };
-    model.Create_Comvolute_Layer(input2, filter2,10);
-    
-    model.Create_Layer(32, RELU);
-    model.Create_Layer(16, SIGMOID);
+    model.Create_Comvolute_Layer(input2, filter2,5,2);
+    int input3[3] = { 4,4,25 };
+    int filter3[3] = { 4,4,1 };
+    model.Create_Comvolute_Layer(input3, filter3, 10, 1);
+    /**/
+    /**/
+    model.Create_Layer(64, RELU);
+    model.Create_Layer(32, SIGMOID);
     model.Create_Layer(10, SOFTMAX);
     model.initialize();
     /**/
-    //model.load_model("mnist_1218_1.tp");
+    //model.load_model("mnist_1226_1.tp");
 
     model.monitor_accuracy(true);
-    model.logging_error("log_mnist1211.csv");
+    //model.logging_error("log_mnist1226.csv");
     model.train(0.01,train_data,test_data,10,10,CROSS_ENTROPY_ERROR);
-    model.save_model("mnist_1218_1.tp");
+    //model.save_model("mnist_1226_1.tp");
     return 0;
 }
